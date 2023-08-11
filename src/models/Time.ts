d27f0e2d54362ad42bfce72d505b39f97dba3f3b
Discobeetle Software <discobeetle.software@gmail.com>
2023-08-10 23:22:31 -0500

    Remove unused code

--- src/models/Time.ts
+++ src/models/Time.ts
@@ -9,31 +9,6 @@ export default class Time {
         this.minute = minute;
     }
 
-    static fromString(input: string | undefined) {
-        let time = new Time();
-        if (input === undefined) {
-            return time;
-        }
-        let values = input.split(':');
-        time.hour = Number(values[0]);
-        time.minute = Number(values[1]);
-        return time;
-    }
-
-    static hydrate(obj: Record<string ,any>): Time {
-        let time = new Time();
-        if (obj === undefined) {
-            return time;
-        }
-        time.hour = obj['hour'];
-        time.minute = obj['minute'];
-        return time;
-    }
-
-    clone(): Time {
-        return new Time(this.minute, this.hour, this.day);
-    }
-
     append(minutes: number) {
         let minute = this.minute + minutes;
         this.minute = minute % 60;
