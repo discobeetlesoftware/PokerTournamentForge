a67c1a9da088bd70a688b84b7aba604f4f7937c1
Discobeetle Software <discobeetle.software@gmail.com>
2023-08-11 18:36:05 -0500

    Allow collapsing the tournament config views

--- src/components/ConfigurableElementView.tsx
+++ src/components/ConfigurableElementView.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useCallback, ChangeEvent } from "react";
+import { Dispatch, useCallback, ChangeEvent, useState, ReactNode } from "react";
 import { SelectorViewProps, SelectorView } from "./SelectorView";
 import { configuration } from "../configuration";
 import Grid, { GridSize } from "@mui/material/Grid";
@@ -7,6 +7,8 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
 export interface Formatable {
     toString(): string;
@@ -41,9 +43,21 @@ export interface ConfigurableElementViewProps<T extends Formatable> {
     format?: (value: T) => string;
 }
 
+const ExpandableElementView = (props: { isOpen: boolean, children: ReactNode }) => {
+    const { isOpen, children } = props;
+    return (
+        <>
+            {isOpen &&
+                <div className='element' >{children}</div>
+            }
+        </>
+    );
+};
+
 export const ConfigurableElementView = <T extends Formatable>(props: ConfigurableElementViewProps<T>) => {
+    const [isVisible, setIsVisible] = useState(true);
     const configs = configuration.defaults.form as Record<string, ConfigurableNumberParams>;
-    const formStrings =  configuration.strings.en.tournament.form as Record<string, ConfigurableElementStrings>;
+    const formStrings = configuration.strings.en.tournament.form as Record<string, ConfigurableElementStrings>;
 
     const { mapKey, grid, update, formElement, defaultElement, selectorViewProps, format } = props;
     const value = formElement[mapKey];
@@ -63,10 +77,11 @@ export const ConfigurableElementView = <T extends Formatable>(props: Configurabl
             <div className='control-group'>
                 <Tooltip title={formStrings[mapKey].tooltip}>
                     <div className='title'>
-                        <Typography color='text.secondary' variant='body1' component='div'>{formStrings[mapKey].name}</Typography>
+                        <Typography color='text.secondary' variant='body1' component='span'>{formStrings[mapKey].name}</Typography>
+                        <Switch size='small' checked={isVisible} onChange={(e, v) => setIsVisible(v)} sx={{marginLeft:'5px'}} />
                     </div>
                 </Tooltip>
-                <div className='element'>
+                <ExpandableElementView isOpen={isVisible}>
                     {fieldTypeString && <TextField
                         color='secondary'
                         fullWidth
@@ -83,24 +98,25 @@ export const ConfigurableElementView = <T extends Formatable>(props: Configurabl
                         }}
                         format={(candidate => {
                             return candidate ? onFormat(candidate) : formStrings[mapKey].placeholder || '';
-                        })} 
+                        })}
                         didSelectValue={candidate => {
-                            update({ [mapKey]: candidate ? selectorViewProps?.formValue(candidate) : undefined })}}
+                            update({ [mapKey]: candidate ? selectorViewProps?.formValue(candidate) : undefined })
+                        }}
                     />}
                     {fieldTypeNumber && <Slider
                         color='secondary'
-                        min={configs[mapKey].min} 
-                        step={configs[mapKey].step} 
-                        max={configs[mapKey].max} 
-                        value={value as number} 
+                        min={configs[mapKey].min}
+                        step={configs[mapKey].step}
+                        max={configs[mapKey].max}
+                        value={value as number}
                         valueLabelDisplay={fieldTypeString ? 'off' : 'auto'}
-                        onChange={(e, v) => update({ [mapKey]: v })} 
+                        onChange={(e, v) => update({ [mapKey]: v })}
                     />}
-                </div>
-                <div className='action'>
-                    <Button color='secondary' variant='contained' size='small' onClick={onReset} disabled={!canReset}>Reset</Button>
-                </div>
-            </div>
+                    <div className='action'>
+                        <Button color='secondary' variant='contained' size='small' onClick={onReset} disabled={!canReset}>Reset</Button>
+                    </div>
+                </ExpandableElementView>
+            </div >
         </Grid>
     )
 };
