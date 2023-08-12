0a7340f78a8c50807f861df16f0b84efac1df7a9
Discobeetle Software <discobeetle.software@gmail.com>
2023-08-11 22:59:45 -0500

    internet says FC is old school

--- src/views/ConfirmDeleteDialog.tsx
+++ src/views/ConfirmDeleteDialog.tsx
@@ -6,7 +6,7 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
-import { FC, forwardRef } from "react";
+import { forwardRef } from "react";
 
 interface ConfirmDeleteDialogProps {
     isOpen: boolean;
@@ -18,9 +18,10 @@ const Transition = forwardRef(function Transition(
     props: TransitionProps & { children: React.ReactElement<any, any> },
     ref: React.Ref<unknown>) {
         return <Slide direction="up" ref={ref} {...props} />;
-});
+    }
+);
 
-export const ConfirmDeleteDialog: FC<ConfirmDeleteDialogProps> = (props) => {
+export const ConfirmDeleteDialog = (props: ConfirmDeleteDialogProps) => {
     const { isOpen, dismiss, message } = props;
     return (
         <Dialog
