45377c0f53d4d77ef38daaf2af8534f1ec0bac48
Discobeetle Software <discobeetle.software@gmail.com>
2023-08-13 21:02:27 -0500

    Add a preset tournament

--- src/pipes/PresetPipes.ts
+++ src/pipes/PresetPipes.ts
@@ -1,4 +1,4 @@
-import { ChipSetPayload } from "../pipes/DataStoreSchemaV1";
+import { ChipSetPayload, TournamentPayload } from "../pipes/DataStoreSchemaV1";
 import { Factory } from "../pipes/Factory";
 import { DataStore, PTDSchemaCurrent } from "../pipes/DataStore";
 import { Color } from "../models/Color";
@@ -36,7 +36,7 @@ function zipChipSet(name: string, offset: number, values: number[], counts: numb
     }
     const date = new Date();
     date.setMilliseconds(date.getMilliseconds() + offset);
-    let set = Factory.chipSet({ name: name, created_at: date, chips: [] }, true);
+    let set = Factory.chipSet({ name: name, created_at: date, chips: [], id: name.toLowerCase().replace(/\s+/g, '') }, true);
     for (let [value, count] of zip(values, counts)) {
         set.chips.push(Factory.chip({
             value: value,
@@ -52,13 +52,297 @@ export const generatePresets = <StoreName extends StoreNames<PTDSchemaCurrent>>(
         case 'settings':
             return [Factory.settings()];
         case 'tournaments':
-            return [];
+            return tournamentPresets();
         case 'chipsets':
             return chipsetPresets();
     }
 
     throw Error(`Unknown table: ${tableName}`);
 }
+
+function zipTournament(offset: number, set_id: string, payload: Partial<TournamentPayload>): TournamentPayload {
+    let tournament = Factory.tournament(payload, true);
+    tournament.set_id = set_id;
+    const date = new Date();
+    date.setMilliseconds(date.getMilliseconds() + offset);
+    tournament.created_at = date;
+    tournament.updated_at = date;
+    return tournament;
+}
+
+
+export const tournamentPresets = () => {
+    return [
+        zipTournament(1, 'default', {
+            "tournament_name": "Two Table",
+            "start_time": 0,
+            "target_duration": 0,
+            "level_duration": 15,
+            "break_duration": 10,
+            "starting_stack": 10000,
+            "player_count": 16,
+            "target_blind_ratio": 0.55,
+            "target_strategy": "AGGRESSIVE",
+            "color_up_breakpoints": [
+                { "denomination": 25, "threshold": 125 },
+                { "denomination": 100, "threshold": 900 },
+                { "denomination": 500, "threshold": 1000 },
+                { "denomination": 1000, "threshold": 6000 },
+                { "denomination": 5000, "threshold": 30000 },
+                { "denomination": 25000, "threshold": 175000 },
+                { "denomination": 100000, "threshold": 600000 },
+                { "denomination": 500000, "threshold": 3500000 },
+                { "denomination": 2500000, "threshold": 15000000 }
+            ],
+            "minimum_denomination": 25,
+            "generator_version": 1,
+            "level_overflow": 3,
+            "break_threshold": [4, 5],
+            "initial_big_blind_multiple": 2,
+            "minimum_color_up_multiple": 2.5,
+            "levels": [
+                {
+                    "id": "febbe6f7-e6c9-42a2-9dbd-2d58f54f9ef6",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        25,
+                        50
+                    ],
+                    "breakOffset": 0
+                },
+                {
+                    "id": "64db1a0a-5f05-448e-8e2a-756028a234c1",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        50,
+                        100
+                    ],
+                    "breakOffset": 0
+                },
+                {
+                    "id": "045888d9-b209-43bd-b53d-4608c4d36af5",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        75,
+                        150
+                    ],
+                    "breakOffset": 0
+                },
+                {
+                    "id": "204a6857-9bf6-4225-bedc-6826fe0285c1",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        125,
+                        250
+                    ],
+                    "breakOffset": 0
+                },
+                {
+                    "id": "c0e491c4-ab51-4802-8f32-b619eaeaadd7",
+                    "type": "break",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "Color up T25",
+                    "description": "",
+                    "duration": 10,
+                    "denominations": [
+                        25
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "c93d9911-67a8-4562-be77-5de938f6ba39",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        200,
+                        400
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "8a0cadbd-dbc0-49d5-8d98-15f0ed45bac3",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        300,
+                        600
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "f7bbd90c-acba-4e0e-b0ef-688f17b88165",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        500,
+                        1000
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "82e67ad3-6319-49d4-94c8-fba739d6224b",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        800,
+                        1600
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "232e1151-58fe-4763-aa3b-aa0f8f62115f",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        1200,
+                        2400
+                    ],
+                    "breakOffset": 1
+                },
+                {
+                    "id": "8fdad12f-ffdf-4a93-9024-edd819389401",
+                    "type": "break",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "Color up T100, T500",
+                    "description": "",
+                    "duration": 10,
+                    "denominations": [
+                        100,
+                        500
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "d33000b5-4993-48be-a2fc-e5f040b75f26",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        2000,
+                        4000
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "f1f2b950-b36c-42ca-9ad9-03ef1a56d752",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        3000,
+                        6000
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "a1c7e269-f107-4903-b420-44d0cbbc6e5b",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": true,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        5000,
+                        10000
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "ba426c5f-b2f8-489c-bf73-465ac4845b6c",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        8000,
+                        16000
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "8c5dce59-47c2-4d94-9516-412379405bd3",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        12000,
+                        24000
+                    ],
+                    "breakOffset": 2
+                },
+                {
+                    "id": "f2d78361-af0a-4d57-89a3-1697e273a3fb",
+                    "type": "round",
+                    "is_keyframe": false,
+                    "is_expected_conclusion": false,
+                    "note": "",
+                    "description": "",
+                    "duration": 15,
+                    "denominations": [
+                        19000,
+                        38000
+                    ],
+                    "breakOffset": 2
+                }
+            ],
+            "is_preset": false
+        })
+    ];
+};
 
 export const chipsetPresets = () => {
     return [
